5f516a3629cd64715373d65ee55d848119feb698
sundaray <pgcim14.hemant@spjimr.org>
2022-08-01 16:27:30 +0530

    Added the Feedback Rating Component

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Card from "./sharedStyling/Card";
 import Button from "./sharedStyling/Button";
+import FeedbackRating from "./FeedbackRating";
 
 const FeedbackForm = () => {
   const [text, setText] = useState("");
+  const [, setRating] = useState(null);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -11,6 +13,11 @@ const FeedbackForm = () => {
   return (
     <Card>
       <h2>How would you rate our service?</h2>
+      <FeedbackRating
+        select={(rating) => {
+          setRating(rating);
+        }}
+      />
       <div className="feedback-form-group">
         <form>
           <input onChange={handleChange} value={text} />
